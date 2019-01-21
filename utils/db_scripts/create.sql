f82a55acf45b69751f0557d2d9715335c0803436
Edimar Calebe  Castanho <calebe.sep@hotmail.com>
2019-01-20 23:22:27 -0200

    feat: added script to create espboy database #wip

--- utils/db_scripts/create.sql
+++ utils/db_scripts/create.sql
@@ -12,48 +12,24 @@
 --
 -- user
 --
-CREATE TABLE user (
-  id INTEGER PRIMARY KEY,
-  username TEXT NOT NULL,
-);
+CREATE TABLE user_info( id INTEGER PRIMARY KEY, username TEXT NOT NULL  );
 
 --
 -- audio
 --
-CREATE TABLE audio (
---   id INTEGER PRIMARY KEY,
-  initialized INTEGER ,     --  Shows if the module was initialized or not during the boot process
-  mod_state INTEGER ,       --  State of the module, it can be ON(1) or OFF(0)
-  volume INTEGER ,
-) WITHOUT ROWID;
+CREATE TABLE audio( initialized INTEGER PRIMARY KEY, mod_state INTEGER, volume INTEGER ) WITHOUT ROWID;
 
 --
 -- wifi
 --
-CREATE TABLE user (
---   id INTEGER PRIMARY KEY,
-  wifi_ssid TEXT,
-  wifi_password TEXT,
-  mod_state TEXT,
-) WITHOUT ROWID;
+CREATE TABLE user ( initialized INTEGER PRIMARY KEY, mod_state TEXT, wifi_ssid TEXT, wifi_password TEXT) WITHOUT ROWID;
 
 --
 -- battery
 --
-CREATE TABLE user (
---   id INTEGER PRIMARY KEY,
-  bat_level INTEGER,
-  ischarging TEXT,
-  mod_state TEXT,
-) WITHOUT ROWID;
+CREATE TABLE battery ( initialized INTEGER PRIMARY KEY, bat_level INTEGER, ischarging TEXT, mod_state TEXT ) WITHOUT ROWID;
 
 --
 -- software
 --
-CREATE TABLE software (
---   id INTEGER PRIMARY KEY,
-  bat_level INTEGER,
-  espidf_version TEXT,
-  app_version TEXT,
-  compile_datetime TEXT,
-) WITHOUT ROWID;
+CREATE TABLE software ( espidf_version TEXT PRIMARY KEY, app_version TEXT, compile_datetime TEXT ) WITHOUT ROWID;
