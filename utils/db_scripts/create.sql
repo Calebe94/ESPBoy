340e6c40faf78a4a5990f9452ca5ba6273b7b424
Calebe94 <calebe.sep@hotmail.com>
2019-01-21 15:26:25 -0200

    feat: changed sqlite database #wip

--- utils/db_scripts/create.sql
+++ utils/db_scripts/create.sql
@@ -9,27 +9,110 @@
     - https://www.sqlite.org/datatype3.html
  */
 
+/***************
+* CREATE TABLES
+***************/
 --
--- user
+-- user: Create the user information.
+-- Can add more than one user to ESPBoy
 --
-CREATE TABLE user_info( id INTEGER PRIMARY KEY, username TEXT NOT NULL  );
+CREATE TABLE IF NOT EXISTS user_info(
+   id INTEGER PRIMARY KEY,
+   username TEXT NOT NULL,          -- User name or user nickname 
+   user_passwd TEXT DEFAULT NULL,   -- User Password, if the user doesn't want a password it should be NULL
+   gender INTEGER,                  -- User gender, 0 to Female, 1 to Male, 2 to other.
+   recent_played TEXT               -- User recently played game.
+);
 
 --
 -- audio
 --
-CREATE TABLE audio( initialized INTEGER PRIMARY KEY, mod_state INTEGER, volume INTEGER ) WITHOUT ROWID;
+CREATE TABLE IF NOT EXISTS audio( 
+   initialized INTEGER PRIMARY KEY, -- Is audio module initiated? (Boolean)
+   mod_state INTEGER,               -- Is audio module ON? (Boolean)
+   volume INTEGER                   -- Audio volume: from 0 to 100
+) WITHOUT ROWID;
 
 --
 -- wifi
 --
-CREATE TABLE user ( initialized INTEGER PRIMARY KEY, mod_state TEXT, wifi_ssid TEXT, wifi_password TEXT) WITHOUT ROWID;
+CREATE TABLE IF NOT EXISTS wifi( 
+   initialized INTEGER PRIMARY KEY, -- Is wifi module initiated? (boolean)
+   mod_state INTEGER,               -- Is wifi connected? (boolean)
+   wifi_ssid TEXT DEFAULT NULL,                  -- Last Wifi ssid connected
+   wifi_password TEXT DEFAULT NULL               -- Last wifi password connected 
+) WITHOUT ROWID;
 
 --
 -- battery
 --
-CREATE TABLE battery ( initialized INTEGER PRIMARY KEY, bat_level INTEGER, ischarging TEXT, mod_state TEXT ) WITHOUT ROWID;
+CREATE TABLE IF NOT EXISTS battery( 
+   initialized INTEGER PRIMARY KEY, -- Is battery module initiated? (boolean)
+   mod_state INTEGER,               -- Is battery charging? (boolean)  
+   bat_level INTEGER                -- Battery level: from 0 to 100%
+) WITHOUT ROWID;
 
 --
 -- software
 --
-CREATE TABLE software ( espidf_version TEXT PRIMARY KEY, app_version TEXT, compile_datetime TEXT ) WITHOUT ROWID;
+CREATE TABLE IF NOT EXISTS software( 
+   espidf_version TEXT PRIMARY KEY, -- ESP-IDF version 
+   app_version TEXT,                -- ESPBoy firmware version
+   compile_datetime TEXT            -- When ESPBoy firmware was compiled
+) WITHOUT ROWID;
+
+/********************
+* INSERT INFO TABLES
+********************/
+--
+-- user
+--
+INSERT INTO user_info (id, username, user_passwd, gender) VALUES (0, 'admin', 'admin', 1);
+
+--
+-- audio
+--
+INSERT INTO audio (initialized, mod_state, volume) VALUES (0, 0, 0);
+
+--
+-- battery
+--
+INSERT INTO battery (initialized, mod_state, bat_level) VALUES (0, 0, 0);
+
+--
+-- wifi
+--
+INSERT INTO wifi (initialized, mod_state, wifi_ssid, wifi_password) VALUES (0, 0, NULL, NULL);
+
+--
+-- software
+--
+INSERT INTO software (espidf_version, app_version, compile_datetime) VALUES ('stable', 'rev0.9', '21-01-2019');
+
+/********************
+* UPDATE INFO TABLES
+********************/
+--
+-- user
+--
+UPDATE user_info SET id=0, username='Guest', user_passwd=NULL, gender=2; 
+
+--
+-- audio
+--
+UPDATE audio SET initialized=1, mod_state=1, volume=100;
+
+--
+-- battery
+--
+UPDATE battery SET initialized=1, mod_state=1 , bat_level=100;
+
+--
+-- wifi
+--
+UPDATE wifi SET initialized=1, mod_state=1, wifi_ssid='Fator', wifi_password='11058283';
+
+--
+-- software
+--
+UPDATE software SET espidf_version='stable',app_version='rev0.9', compile_datetime='21-01-2019';
